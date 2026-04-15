1006427b309422b291d70d83b86013fb0a3bf3ed
Harvey <99469911+harvey-euph@users.noreply.github.com>
2026-04-16 01:50:40 +0800

    Update loader.c

--- my_scx/loader.c
+++ my_scx/loader.c
@@ -1,50 +1,30 @@
-// SPDX-License-Identifier: GPL-2.0 OR BSD-3-Clause
+// loader.c
 #include <stdio.h>
 #include <unistd.h>
-#include <signal.h>
 #include <bpf/libbpf.h>
-#include "minimal.skel.h"   // 之後 clang + bpftool 會產生
+#include "sched_ext.skel.h"
 
-static volatile bool exiting = false;
-
-static void sig_handler(int sig)
+int main()
 {
-    exiting = true;
-}
+    struct sched_ext_bpf *skel;
 
-int main(int argc, char **argv)
-{
-    struct minimal_bpf *skel;
-    int err;
-
-    signal(SIGINT, sig_handler);
-    signal(SIGTERM, sig_handler);
-
-    skel = minimal_bpf__open();
+    skel = sched_ext_bpf__open_and_load();
     if (!skel) {
-        fprintf(stderr, "Failed to open skeleton\n");
+        fprintf(stderr, "Failed to open/load BPF\n");
         return 1;
     }
 
-    err = minimal_bpf__load(skel);
-    if (err) {
-        fprintf(stderr, "Failed to load BPF skeleton\n");
-        goto cleanup;
+    if (sched_ext_bpf__attach(skel)) {
+        fprintf(stderr, "Failed to attach\n");
+        return 1;
     }
 
-    err = minimal_bpf__attach(skel);
-    if (err) {
-        fprintf(stderr, "Failed to attach scheduler\n");
-        goto cleanup;
+    printf("sched_ext program loaded!\n");
+
+    while (1) {
+        sleep(1);
     }
 
-    printf("minimal_scx scheduler 已經啟動！按 Ctrl+C 停止。\n");
-    printf("可以用 `cat /sys/kernel/debug/tracing/trace_pipe` 看 log\n");
-
-    while (!exiting)
-        pause();
-
-cleanup:
-    minimal_bpf__destroy(skel);
-    return err < 0 ? -err : 0;
+    sched_ext_bpf__destroy(skel);
+    return 0;
 }
