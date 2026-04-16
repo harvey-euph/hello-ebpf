319c2445110266f62c06f83a94e094aa720940ae
Harvey <harvey.euph@gmail.com>
2026-04-16 19:05:06 +0000

    updates

--- scx/setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# Generate the vmlinux.h file to start using in the IDE
-
-bpftool btf dump file /sys/kernel/btf/vmlinux format c > vmlinux.h
--- scx/stop.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# Remove the scheduler
-sudo rm /sys/fs/bpf/sched_ext/sched_ops
