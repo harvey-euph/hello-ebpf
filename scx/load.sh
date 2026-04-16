31bb193241ee01af37aabc3aca50f4896c046601
Harvey <99469911+harvey-euph@users.noreply.github.com>
2026-04-17 02:49:10 +0800

    Update and rename load.sh to load

--- scx/load.sh
+++
@@ -1,8 +0,0 @@
-OPS=$(cat /sys/kernel/sched_ext/root/ops 2>/dev/null || echo "")
-
-if [ -n "$OPS" ]; then
-    echo "[INFO] scheduler original running: $OPS, stopping..."
-    sudo ./stop.sh
-fi
-
-sudo bpftool struct_ops register $1 /sys/fs/bpf/sched_ext
